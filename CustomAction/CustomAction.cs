cdb62a93db2d20bb57664b0cf5b1687e747cf66d
ANDREIMUSAT\Andrei <andrei.musat@assist.ro>
2018-07-24 16:29:58 +0300

    CustomAction Added

---
+++ CustomAction/CustomAction.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Deployment.WindowsInstaller;
+
+namespace CustomAction
+{
+    public class CustomActions
+    {
+        [CustomAction]
+        public static ActionResult CustomAction1(Session session)
+        {
+            session.Log("Begin CustomAction");
+            return ActionResult.Success;
+        }
+    }
+}
